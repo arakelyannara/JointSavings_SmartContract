75bca0c9a68770ca3cc1288e17f66149f5fa6dfb
Nara Arakelyan <nara@Naras-MacBook-Pro.local>
2022-02-20 21:14:01 -0800

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # JointSavings_SmartContract
+
+In this repo we will automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+
+
+## Technologies
+We will using Remix IDE open source web application to compile, deploy and test our Smart Contract. 
+
+* <https://remix.ethereum.org/>
+
+
+ 
+## Usage
+
+* Clone the repository.
+
+* Open it in Remix IDE
+
+For demonstration purposes we use JavaScript VM sandbox to deploy aor SmartContract and 2 Ethereum dummy addresses from Vanity-Eth vanity address generator.
+
+See the video below how it works
+
+[SmartContract in action](ExecutionResults/Recording.mov)
+
+When the transaction is successful the hash will be created:
+
+There are some transaction made with this Smart contract the resulting pictures of which you can find in Execution result folder.
+
+Transaction 1: Send 1 ether as wei.
+![](ExecutionResults/result_1.png)
+Transaction 2: Send 10 ether as wei.
+![](ExecutionResults/result_1.png)
+Transaction 3: Send 5 ether.
+![](ExecutionResults/result_1.png)
+    Set accounts one and Two
+![](ExecutionResults/set_accounts.png)
+Withdrawing 5 ether into accountOne. 
+![](ExecutionResults/result_1.png)
+Withdrawing 10 ether into accountTwo.
+![](ExecutionResults/result_1.png)
+
+
+## Contributor
+
+Nara Arakelyan
+
+---
+
+## Licence 
+
+UC Berkley
+    
---
+++ joint_savings.sol
@@ -0,0 +1,72 @@
+/*
+Joint Savings Account
+---------------------
+
+Automation of the creation of joint savings accounts. 
+A solidity smart contract that accepts two user addresses that are then able to control a joint savings account. 
+The smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+
+---------------------
+We will use the following Ethereum dummy addresses on the Vanity-ETH website, which includes an Ethereum vanity address generator.
+
+  Dummy account1 address: 0x0c0669Cd5e60a6F4b8ce437E4a4A007093D368Cb
+  Dummy account2 address: 0x7A1f3dFAa0a4a19844B606CD6e91d693083B12c0
+*/
+
+pragma solidity ^0.5.0;
+
+// Define a new contract named `JointSavings`
+contract JointSavings {
+    /*
+    Define the following variables in the new contract:
+      - Two variables of type `address payable` named `accountOne` and `accountTwo`
+      - A variable of type `address public` named `lastToWithdraw`
+      - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`
+    */
+  address payable accountOne;
+  address payable accountTwo;
+  address public lastToWithdraw;
+  uint public lastWithdrawAmount; 
+  uint public contractBalance;
+    
+    /*
+    Define a function named `withdraw` that accepts two arguments:
+      - An `amount` of type `uint`
+      - A `recipient` of type `payable address`
+      
+    */
+
+  function withdraw(uint amount, address payable recipient) public {
+    require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
+    require(amount <= contractBalance, "Insufficient funds!");
+    
+    if (lastToWithdraw != recipient) {
+      lastToWithdraw = recipient;
+    }
+      
+    recipient.transfer(amount);
+    lastWithdrawAmount = amount;
+    contractBalance = address(this).balance;
+    }
+
+  // Define a `public payable` function named `deposit`
+  function deposit() public payable {
+
+    contractBalance = address(this).balance;
+  }
+   
+   /*
+    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
+    */
+  function setAccounts(address payable account1, address payable account2) public{
+
+    accountOne = account1;
+    accountTwo = account2;
+  }
+   
+   /*
+    Add a fallback function so that your contract can store ether that's sent from outside the deposit function
+    */
+  function() external payable {}
+
+}
